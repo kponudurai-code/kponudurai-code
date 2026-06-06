d68a5821dbce92c263a3a7cc049780bcf3682d27
kponudurai-code <karthik.ponudurai@gmail.com>
2026-06-06 18:28:19 +0100

    removed invalid test input = 1.5

--- tests/test_device_api.py
+++ tests/test_device_api.py
@@ -1,7 +1,7 @@
 import pytest
 from libraries.device_api import get_post, POST_ID
 
-@pytest.mark.parametrize("post_id", [1,2,3,1.5,6])
+@pytest.mark.parametrize("post_id", [1,2,3,6])
 def test_get_post(post_id):
     
     response = get_post(post_id)
